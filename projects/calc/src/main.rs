01a44191eff147cac6644f1f1f705a6ab0c587e5
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-26 10:37:42 -0300

    update calc project

--- projects/calc/src/main.rs
+++ projects/calc/src/main.rs
@@ -1,3 +1,12 @@
+fn soma(x: i32, y: i32) -> i32 {
+    x + y
+}
+
 fn main() {
+    let _name = "Mateus";
+    let _idade = 18i64;
+    let _cores = vec!["red", "green"];
+
+    soma(32, 45);
     println!("Hello, world!");
 }
