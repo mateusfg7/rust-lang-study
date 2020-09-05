5156ab43ef749d586f2333740e0e1d1cb9657266
mateusfg7 <mateusfelipefg77@gmail.com>
2020-09-05 09:42:41 -0300

    If Else Statements

---
+++ rust-programmin-tutorial/06/if-statement/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let n = 56;
+
+    if n == 45 {
+        println!("Rust");
+    } else if n > 50 {
+        println!("It is greater than 50");
+    } else {
+        println!("It was not 45 and it was less than 50");
+    }
+}
